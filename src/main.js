bf9fb5642fc8e2f15294e32cc39b4c2acd2aa520
Heryck <heryckmota@gmail.com>
2024-01-19 17:46:47 -0300

    bagucna

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import PrimeVue from 'primevue/config';
 
 const app = createApp(App)
+app.use(PrimeVue);
 
 app.use(router).use(store)
 
--- src/views/PokemonDetail.vue
+++ src/views/PokemonDetail.vue
@@ -1,27 +1,71 @@
 <template>
+    <div class="main-container">
+        <div class="presentation">
+            <h2>#{{ pokemon.id }}</h2>
+            <h1>{{ transformaCamelCase(pokemon.name) }}</h1>
+            <div class="badge genera">
+                {{ generaPokemon }}
+            </div>
+            <div class="image">
+
+                <img :src="pokemon.sprites.other['dream_world'].front_default" alt="">
+
+            </div>
+            <div class="typeList">
+                <div :class="['badge', item.type.name]" v-for="item in pokemon.types">
+                    {{ transformaCamelCase(item.type.name) }}
+                </div>
+            </div>
+            <p><b>Height</b> {{ pokemon.height / 10 }} m</p>
+            <p><b>Weight</b> {{ pokemon.weight / 10 }} Kg</p>
+            <p>Male : {{ getGender.male }}</p>
+            <p>Female :{{ getGender.female }}</p>
+            <div class="genderRate">
+
+            </div>
+        </div>
+        <div class="infos">
+            <div>
+                <h2>About</h2>
+                <div class="about">
+                    {{ descriptionPokemon }}
+                </div>
+            </div>
+
+            <div class="combat">
+                <div class="doubleDamageContainer">
+                    <h2>Double damage To</h2>
+                    <div class="about doubleDamage">
+                        <div :class="[index, 'badge']" v-for="(item, index) in strongAgainst">
+                            {{ transformaCamelCase(index) }}
+                        </div>
+                    </div>
+                </div>
+                <div class="doubleDamageContainer">
+                    <h2>Double damage To</h2>
+                    <div class="about doubleDamage">
+                        <div :class="[index, 'badge']" v-for="(item, index) in strongAgainst">
+                            {{ transformaCamelCase(index) }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <ProgressBar :value="40"> {{ 10 }}/100 </ProgressBar>
+
+        </div>
+    </div>
     <div class="container name">
         <h1>{{ transformaCamelCase(pokemon.name) }} - N° {{ pokemon.id }}</h1>
     </div>
 
     <div class="container " id="pokemon">
         <div class="content">
-            <div class="image">
 
-                <img :src="pokemon.sprites.other['dream_world'].front_default" alt="">
-
-            </div>
             <div class="column">
                 <div class="carateristicas dog-ear">
 
                     <div class="info">
-                        <div class="">
-                            <label for="">Height</label>
-                            <p>{{ pokemon.height / 10 }} m</p>
-                        </div>
-                        <div class="">
-                            <label for="">Weight</label>
-                            <p>{{ pokemon.weight / 10 }} Kg</p>
-                        </div>
+
                         <div class="">
                             <label for="">Habitat</label>
                             <p>{{ transformaCamelCase(detalhesEspecie.habitat.name) }} </p>
@@ -32,31 +76,20 @@
                         </div>
                     </div>
 
-                    <div class="types">
-                        <label>Types</label>
-                        <div class="typeList">
-                            <div :class="['badge', item.type.name]" v-for="item in pokemon.types">
-                                {{ transformaCamelCase(item.type.name) }}
-                            </div>
-                        </div>
-                    </div>
+
 
                     <div class="" style="display: flex;">
                         <div class="types">
                             <label>Strong against</label>
                             <div class="mini-grid">
-                                <div :class="['', 'grid-item', index]" v-for="(item, index) in strongAgainst"
-                                    class="typeList">
-                                    {{ transformaCamelCase(index) }}
-                                </div>
+
                             </div>
                         </div>
 
                         <div class="types">
                             <label>Weak against</label>
                             <div class="mini-grid">
-                                <div :class="['', 'grid-item', index]" v-for="(item, index) in weakAgainst"
-                                    class="typeList">
+                                <div :class="['', 'grid-item', index]" v-for="(item, index) in weakAgainst">
                                     {{ transformaCamelCase(index) }}
                                 </div>
                             </div>
@@ -117,9 +150,16 @@
     <div class="container">
         <h1>Galeria</h1>
         <div class="galery">
-            <img width="150%" v-for="image in spriteImages" :key="image" :src="image" alt="">
-            <img width="100%" v-for="image in showdownImages" :key="image" :src="image" alt="">
-            <img width="100%" v-for="image in mainArtsImages" :key="image" :src="image" alt="">
+            <div class="centered" v-for="image in spriteImages" :key="image">
+                <img width="50%" :src="image" alt="">
+
+            </div>
+            <div v-for="image in showdownImages" :key="image" class="centered">
+                <img width="50%" :src="image" alt="">
+            </div>
+            <div class="centered" v-for="image in mainArtsImages" :key="image">
+                <img width="50%" :src="image" alt="">
+            </div>
 
         </div>
         <div class="galery"></div>
@@ -128,189 +168,182 @@
         </div>
     </div>
 </template>
+
+
 <style scoped>
-.galery {
-    display: grid;
-    grid-template-areas: 'item item item item ';
-    justify-content: space-around;
-    width: 95%;
-}
-
-.name {
-    width: 30% !important;
-    color: white;
-    padding: 1% !important;
-
-}
-
-.strongAgainst {
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-
-    grid-gap: 10px;
-}
-
-.mini-grid {
-    display: grid;
-    max-width: 85%;
-    margin-top: 2%;
-    grid-template-areas: 'item item item ';
-    gap: 10px;
-}
-
-.grid-item {
-    grid-area: 'item';
+.about {
+    background-color: #1A1A1A;
+    padding: 2%;
+    margin-top: 10px;
     border-radius: 10px;
-    padding: 12%;
-    max-width: 100%;
-    margin-right: 2%;
-    background-color: #D9DAD9;
-    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.8);
-    color: white;
+    height: 100%;
 }
 
-.column {
+.combat {
     display: flex;
-    flex-direction: column;
-    width: 50%;
+
+}
+.shell {
+  height: 20px;
+  width: 250px;
+  border: 1px solid #aaa;
+  border-radius: 13px;
+  padding: 3px;
 }
 
-.content {
+.bar {
+  background: linear-gradient(to right, #B993D6, #8CA6DB);
+  height: 20px;
+  width: 15px;
+  border-radius: 9px;
+ 
+}
+
+.bar span {
+    float: right;
+    padding: 4px 5px;
+    color: #fff;
+    font-size: 0.7em
+  }
+
+.doubleDamage {
+    display: grid;
+    grid-template-areas: 'item item';
+    gap: 5%;
+    padding: 8%;
+
+}
+
+.doubleDamageContainer {
+    margin-right: 5%;
+    width: 30%;
+}
+
+.statusContainer {
+    width: 60%;
+}
+
+.typeList {
+
+    margin-top: 9%;
     display: flex;
-    width: 100%;
-    justify-content: space-around;
+    justify-content: center;
+    gap: 10%;
+}
+
+.doubleDamage {
+    margin-right: 5%;
+}
+
+.genera {
+    background-color: red;
+    padding: 4% !important;
 }
 
 .badge {
-    border-radius: 10px;
-    padding: 2% 3.5%;
-    margin-right: 2%;
-    background-color: #6791B9;
-    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.8);
-    color: white;
-
-}
-
-.carateristicas {
-    height: 100%;
-    width: 100%;
-    margin: 10px;
-    background-color: #121212;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    padding: 1%;
-}
-
-.image {
-    width: 35%;
-    padding: 2%;
-    background-color: #121212;
-    box-shadow: 0 7px 15px rgba(0, 0, 0, 0.8);
+    padding: 17%;
     border-radius: 10px;
     display: flex;
     justify-content: center;
     align-items: center;
 }
 
-.image img {
-    width: 80%;
-    height: 80%;
-    border-radius: 10px;
-}
 
-
-.info {
-    margin: 10px;
-    justify-content: start;
-    display: flex;
-    width: 100%;
-}
-
-.typeList {
-    justify-content: start;
-    display: flex;
-    margin-top: 1%;
-}
-
-.types {
-    margin: 10px;
-
-    width: 100%;
-}
-
-.info div {
-    margin-right: 4%;
-}
-
-.info p {
-    font-size: 13pt;
-    color: white;
-}
-
-.carateristicas label {
-    font-size: 18pt;
-    color: #ED1E24;
-}
-
-.container {
-
-    width: 85%;
-    height: 100%;
-    border: 100px;
-
-    margin: 10px auto;
-    padding: 2%;
-    border-radius: 10px;
+.main-container {
+    width: 90%;
     background-color: #242424;
+    margin: 0 auto;
+    padding: 1%;
+    justify-content: center;
+    display: flex;
+    gap: 2%;
+}
 
+.presentation {
+    width: 15%;
     display: flex;
     flex-direction: column;
-
+    justify-content: center;
+    align-items: center;
+    background-color: #1A1A1A;
+    border-radius: 10px;
 }
 
-.dog-ear {
-    position: relative;
-
+.image {
+    margin-top: 10%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 }
 
-.dog-ear::after {
-    content: "";
-    position: absolute;
-    bottom: 0;
-    right: 0;
-    border: 20px solid transparent;
-    border-right-color: #242424;
-    border-bottom-color: #242424;
-    box-sizing: border-box;
+.image img {
+    width: 70%;
+}
+
+.infos {
+    width: 75%;
 }
 </style>
 
 <script>
 import Api from '@/http/Api';
 import { VueperSlides, VueperSlide } from 'vueperslides'
-import 'vueperslides/dist/vueperslides.css'
+import ProgressBar from 'primevue/progressbar';
 
 export default {
 
     computed: {
+        getGender() {
+            let genderRate = this.detalhesEspecie.gender_rate;
+            switch (genderRate) {
+                case 0:
+                    return { 'male': 100, 'female': 0 };
+                case 1:
+                    return { 'male': 87.5, 'female': 12.5 };
+                case 2:
+                    return { 'male': 75, 'female': 25 };
+                case 3:
+                    return { 'male': 62.5, 'female': 37.5 };
+                case 4:
+                    return { 'male': 50, 'female': 50 };
+                case 5:
+                    return { 'male': 37.5, 'female': 62.5 };
+                case 6:
+                    return { 'male': 25, 'female': 75 }
+                case 7:
+                    return { 'male': 12.5, 'female': 87.5 };
+                case 8:
+                    return { 'male': 0, 'female': 100 };
+                default:
+                    return { 'make': 0 };
+            }
+        },
+        descriptionPokemon() {
+            let description = this.detalhesEspecie.flavor_text_entries.filter((element) => element.language.name == 'en')[0].flavor_text
+            return description
+        },
+        generaPokemon() {
+            let genera = this.detalhesEspecie.genera.filter((element) => element.language.name == 'en')[0].genus
+            return genera
+        },
         spriteImages() {
             let images = this.filtrarNaoNulos(this.pokemon.sprites);
             return images;
         },
-        showdownImages(){
+        showdownImages() {
             return this.filtrarNaoNulos(this.pokemon.sprites.other.showdown);
         },
-        mainArtsImages(){
+        mainArtsImages() {
             let imagesDream = this.filtrarNaoNulos(this.pokemon.sprites.other.dream_world);
             let home = this.filtrarNaoNulos(this.pokemon.sprites.other.home);
             let oficialArt = this.filtrarNaoNulos(this.pokemon.sprites.other['official-artwork']);
-            return [...Object.values(imagesDream),...Object.values(home),...Object.values(oficialArt)]
+            return [...Object.values(imagesDream), ...Object.values(home), ...Object.values(oficialArt)]
         }
 
     },
     components: {
-        VueperSlides, VueperSlide
+        ProgressBar
     },
 
     data() {
